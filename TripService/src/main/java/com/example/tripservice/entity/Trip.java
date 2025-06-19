bf466d32798c376a81d2de4c53d6952b1de10be4
Valdes Voufo <valdes.voufo.spam@gmail.com>
2025-06-19 17:33:50 +0200

    init

--- TripService/src/main/java/com/example/tripservice/entity/Trip.java
+++
@@ -1,9 +0,0 @@
-package com.example.tripservice.entity;
-
-public class Trip {
-    String departureDate;
-    String destinationCity;
-    String departureCity;
-    int availableWeight;
-
-}
--- TripService/src/main/java/com/example/tripservice/repository/TripRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tripservice.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.tripservice.entity.Person;
-import com.example.tripservice.entity.Trip;
-
-public interface TripRepository extends  JpaRepository<Trip, Long>{
-    // Méthodes de requête personnalisées, si nécessaire
-    Trip findByTrip(String trip);
-}
